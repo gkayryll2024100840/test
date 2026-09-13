0d510bfc3573ce527d2fda4ca4d7f2378491e402
gkayryll2024100840 <gkayryll@gmail.com>
2026-09-13 08:13:05 +0000

    Added unauthorized access attempts

--- login.py
+++ login.py
@@ -88,7 +88,6 @@ def verify_login(user_id, password):
                 if st.session_state.failed_attempts >= MAX_FAILED_ATTEMPTS:
                     log_failed_attempt(user['UserID'], st.session_state.get('client_ip'))
                     return False, "Invalid User ID or password. Unauthorized attempts have been logged."
-                    st.write(f"🐛 Debug — failed_attempts: {st.session_state.failed_attempts}")
                 else: 
                     return False, "Invalid User ID or password. Verify using email."
                 
