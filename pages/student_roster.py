c51ac1545648ddeb5378764c339082a3d73140bd
jillrueda01 <jillrueda01@gmail.com>
2026-09-13 19:02:42 +0800

    Update student roster page and sync files

--- pages/student_roster.py
+++ pages/student_roster.py
@@ -1,5 +1,7 @@
+from attrs import field
 import streamlit as st
-from db_connect import get_students_data, get_last_updated_time, trigger_data_sync, get_max_retry_count
+import re
+from db_connect import get_student_roster_data, get_last_updated_time, trigger_data_sync, get_max_retry_count
 
 st.set_page_config(page_title="Student Roster", layout="wide")
 
@@ -7,11 +9,9 @@ st.set_page_config(page_title="Student Roster", layout="wide")
 active_login_id = st.session_state.session_id
 
 # Check repeated failures specifically for THIS session
-#lowk idk if we need this one
 retry_count = get_max_retry_count(active_login_id)
 if retry_count > 1:
     st.warning(f"**Warning:** Repeated sync failures detected for your session ({retry_count} attempts). Check Admin logs.")
-#maybe add a limit of 20 attempts...
 
 #Splits the top section into two columns. Wider one for the title, smaller one for timestamp 
 col_title, col_action = st.columns([2, 1])
@@ -35,8 +35,29 @@ with col_action:
 st.markdown("---")
 
 #displays student roster in table format
-df = get_students_data()
+df = get_student_roster_data()
+
 if not df.empty:
-    st.dataframe(df, use_container_width=True)
+  #sorting map (only these columns can be the basis of sorting the table)
+  sort_map = {
+      "Student ID": "StudentNumber",
+      "Student": "Student",
+      "Enrollment Status": "EnrollmentStatus",
+  }
+
+  #dropdown selector
+  sort_choice = st.selectbox("SORT BY:", list(sort_map.keys()))
+
+  df_sorted = df.sort_values(by=sort_map[sort_choice])
+
+  #formatted column headers (ex: StudentNumber -> STUDENT NUMBER)
+  df_display = df_sorted.rename(
+      columns=lambda x: re.sub(r"(?<!^)(?=[A-Z])", " ", x).upper()
+  )
+
+  st.dataframe(df_display, use_container_width=True)
+
 else:
-    st.info("No student records found in the database or connection issue occurred.")
+  st.info(
+      "No student records found in the database or connection issue occurred."
+  )
