84a2c8b60a82b07fcec2dda2f7ad6765e3b5be5d
gkayryll2024100840 <gkayryll@gmail.com>
2026-09-17 20:26:51 +0800

    Update student_roster.py

--- dashboard_views/student_roster.py
+++ dashboard_views/student_roster.py
@@ -70,7 +70,7 @@ if not df.empty:
         search_query = st.text_input(
             "SEARCH:",
             placeholder="Search by student name or ID...",
-            label_visibility="visible"
+            label_visibility="visible",
             use_container_width=True
         )
 
